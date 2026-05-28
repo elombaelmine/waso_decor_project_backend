2f4b4e2a0e5f7376cdfd2e552fd0af1116326587
Elomba Elmine <elmine0520@gmail.com>
2026-05-28 06:54:27 +0100

    Configure Cloudinary storage and config in settings.py

--- core/settings.py
+++ core/settings.py
@@ -212,5 +212,12 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUD_API_SECRET'),
 }
 
+# --- ADD THIS BLOCK ---
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUD_NAME'),
+    api_key=os.environ.get('CLOUD_API_KEY'),
+    api_secret=os.environ.get('CLOUD_API_SECRET'),
+)
+
 # This tells Django to use Cloudinary for file uploads
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
